3eb77dcd896cbb97af7858f6419d8a9f1a3966a5
cgen101 <chloe.gentry@gmail.com>
2024-09-20 12:46:32 -0500

    circuit design

---
+++ circuit-design.v
@@ -0,0 +1,24 @@
+module not_gate (Y, A);
+    output Y;
+    input A;
+    assign Y = !A; 
+endmodule
+
+module nand_gate (Y, A, B);
+    output Y; 
+    input A, B; 
+    assign Y= !(A & B);
+endmodule
+
+module nor_gate (Y, A, B);
+    output Y; 
+    input A, B;
+    assign Y = !(A | B);
+endmodule
+
+module shift_4bit (Y, A);
+    output [3:0] Y;
+    input [3:0] A;
+    assign Y = A << 1;
+endmodule
+
