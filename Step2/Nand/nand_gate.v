052dabb24984db646b80708da9c3e9f023f5935b
cgen101 <chloe.gentry@gmail.com>
2024-10-27 10:10:21 -0500

    nand tb

--- Step2/Nand/nand_gate.v
+++ Step2/Nand/nand_gate.v
@@ -1,11 +1,11 @@
 `ifndef NAND-GATE_V
-`define NAND-GATE_V
+`define NAND_GATE_V
 
 module nand_gate (Y, A, B);
     output [3:0] Y;  // 4-bit output
     input [3:0] A, B;  // 4-bit inputs
 
-    assign Y = ~(A & B);  // Perform bitwise AND and negate it
+    assign Y = !(A & B);  
 endmodule
 
 `endif
